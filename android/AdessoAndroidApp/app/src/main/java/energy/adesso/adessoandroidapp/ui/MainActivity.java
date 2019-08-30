8b5305a405147458835385d1fcf2e2dff2e453e0
Niklas Carstensen <stu211271@uni-kiel.de>
2019-08-30 14:46:12 +0200

    Find out how to do lists in android studio again

--- android/AdessoAndroidApp/app/src/main/java/energy/adesso/adessoandroidapp/ui/MainActivity.java
+++ android/AdessoAndroidApp/app/src/main/java/energy/adesso/adessoandroidapp/ui/MainActivity.java
@@ -3,7 +3,13 @@ package energy.adesso.adessoandroidapp.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import energy.adesso.adessoandroidapp.R;
@@ -15,5 +21,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((Button)findViewById(R.id.button)).setText(MainController.getInstance().doStuff());
+    }
+
+    TextView CreateTextview(String text)
+    {
+        TextView t = new TextView(this);
+        t.setText(text);
+        return t;
+    }
+
+    public void onButtonClick(View view)
+    {
+        View childLayout = getLayoutInflater().inflate(R.layout.list_element, null);
+        ((LinearLayout)findViewById(R.id.lay)).addView(childLayout);
     }
 }
