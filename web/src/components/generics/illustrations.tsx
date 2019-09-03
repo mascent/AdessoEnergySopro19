e7d03710ce01179a9e2a8ee769403db76b362a40
Christoph Fricke <christoph@frickeonline.de>
2019-09-03 11:52:11 +0200

    Make SVG always try to fill their box

--- web/src/components/generics/illustrations.tsx
+++ web/src/components/generics/illustrations.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import cx from 'classnames';
+import styles from './illustrations.module.scss';
 import { ReactComponent as Logo } from './assets/auth.svg';
 import { ReactComponent as Blanket } from './assets/paper.svg';
 
@@ -11,9 +13,19 @@ interface IllustrationProps {
 const Illustrations: React.FC<IllustrationProps> = ({ type, className }) => {
   switch (type) {
     case 'Auth':
-      return <Logo data-testid="Auth" className={className} />;
+      return (
+        <Logo
+          data-testid="Auth"
+          className={cx(styles.illustration, className)}
+        />
+      );
     case 'NoSelected':
-      return <Blanket data-testid="NoSelected" className={className} />;
+      return (
+        <Blanket
+          data-testid="NoSelected"
+          className={cx(styles.illustration, className)}
+        />
+      );
     default:
       throw new TypeError('Unknown Illustrations type.');
   }
