888e7575bb90e0efffeef8cb10db319cad86a8c6
Vincent Shahriyari <vincent@innovandalism.eu>
2019-09-03 10:04:26 +0200

    Update to new version

---
+++ spring/sopro-spring-project-test/src/test/java/de/sopro/controller/PictureControllerTest.java
@@ -0,0 +1,19 @@
+package de.sopro.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PictureControllerTest {
+
+	@Autowired
+	MockMvc mvc;
+	
+}
