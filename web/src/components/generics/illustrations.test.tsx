4a712818f6140fd866733c23364ff2428e681699
Tammo Brüggemann <stu209220@mail.uni-kiel.de>
2019-09-03 10:18:48 +0200

    add test for illustration

--- web/src/components/generics/illustrations.test.tsx
+++ web/src/components/generics/illustrations.test.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import Logo, { IllustrationType } from './illustrations';
+import { render } from '@testing-library/react';
 
-test('Dully', () => {
-  expect(1 + 1).toBe(2);
+const types: IllustrationType[] = ['Auth', 'NoSelected'];
+
+test.each(types)('Logo of type %s matches with snapshot', type => {
+  const { getByTestId } = render(<Logo type={type} />);
+  const logo = getByTestId(type);
+  expect(logo).toMatchSnapshot();
 });
