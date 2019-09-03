caf27bd449bd23d3a36224e4487f4e11efc4f099
Vincent Shahriyari <vincent@innovandalism.eu>
2019-09-03 21:43:40 +0200

    Updated Test so they could actually run

--- spring/spring-project-backend/src/test/java/de/sopro/controller/ReadingControllerTest.java
+++ spring/spring-project-backend/src/test/java/de/sopro/controller/ReadingControllerTest.java
@@ -1,16 +1,21 @@
 package de.sopro.controller;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
+import de.sopro.TestConfig;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ContextConfiguration(classes = TestConfig.class)
+@ContextConfiguration(classes = TestConfig.class)
 public class ReadingControllerTest {
 	
 	@Autowired
--- spring/spring-project-backend/src/test/java/de/sopro/soprospringproject/SoproSpringProjectApplicationTests.java
+++ spring/spring-project-backend/src/test/java/de/sopro/soprospringproject/SoproSpringProjectApplicationTests.java
@@ -1,12 +1,19 @@
 package de.sopro.soprospringproject;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
+import de.sopro.TestConfig;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ContextConfiguration(classes = TestConfig.class)
 public class SoproSpringProjectApplicationTests {
 
 	@Test
