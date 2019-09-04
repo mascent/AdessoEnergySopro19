3324543aa9eb09d3e3f444aa1aec4f39c066f72f
Richard Hanß <33845166+finia2NA@users.noreply.github.com>
2019-09-04 12:04:03 +0200

    add Base64 encoder in RequestBuilder

--- android/AdessoAndroidApp/app/src/main/java/energy/adesso/adessoandroidapp/logic/controller/RequestBuilder.java
+++ android/AdessoAndroidApp/app/src/main/java/energy/adesso/adessoandroidapp/logic/controller/RequestBuilder.java
@@ -1,5 +1,14 @@
 package energy.adesso.adessoandroidapp.logic.controller;
 
-public class RequestBuilder {
+import android.graphics.Bitmap;
+import android.util.Base64;
 
+import java.io.ByteArrayOutputStream;
+
+public class RequestBuilder {
+  private String toBase64(Bitmap image){
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    image.compress(Bitmap.CompressFormat.PNG, 100,baos);
+    return Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+  }
 }
