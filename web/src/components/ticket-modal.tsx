982a71c8b29bffd6df28f6d9cf9b0a2e2cf55edc
Tammo Brüggemann <stu209220@mail.uni-kiel.de>
2019-09-04 13:23:28 +0200

    add ticket-modal without logic and tests

---
+++ web/src/components/ticket-modal.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import { Title } from './generics/text';
+import { PrimaryButton, SecondaryButton } from './generics/button';
+import Input from './generics/input';
+import Modal, { ModalBody, ModalFooter } from './generics/modal';
+import { useInputValidation } from 'use-input-validation';
+import styles from './ticket-modal.module.scss';
+
+interface TicketModalProps {
+  onSend: (
+    name: string,
+    email: string,
+    subject: string,
+    message: string
+  ) => void;
+  isOpen: boolean;
+  closeModal: () => void;
+}
+
+const TicketModal: React.FC<TicketModalProps> = ({
+  closeModal,
+  isOpen,
+  onSend
+}) => {
+  const [value, setValue] = React.useState([]);
+
+  const ref = React.useRef(null);
+
+  const name = useInputValidation('', 'Kein valider Name', value => true);
+
+  const email = useInputValidation(
+    '',
+    'Keine valide Email-Adresse',
+    value => true
+  );
+
+  const subject = useInputValidation('', 'Kein valider Betreff', value => true);
+
+  const message = useInputValidation(
+    '',
+    'Keine valide Nachricht',
+    value => true
+  );
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const nameIdValid = name.validate();
+    const emailValid = email.validate();
+    const subjectIDValid = subject.validate();
+    const emailIDValid = email.validate();
+    const messageIDValid = message.validate();
+
+    if (
+      !(
+        nameIdValid &&
+        emailValid &&
+        subjectIDValid &&
+        emailIDValid &&
+        messageIDValid
+      )
+    )
+      return;
+
+    onSend(name.value, email.value, subject.value, message.value);
+  }
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      title="Support Ticket erstellen"
+      defaultFocusRef={ref}
+      onModalCloseRequest={closeModal}
+    >
+      <form onSubmit={handleSubmit}>
+        <ModalBody>
+          <Input
+            id="nameID"
+            type="text"
+            label="Name"
+            value={name.value}
+            onChange={value => name.setValue(value)}
+            onBlur={name.validate}
+            error={name.error}
+          />
+
+          <Input
+            id="Email"
+            label="Email"
+            type="text"
+            value={email.value}
+            onChange={value => email.setValue(value)}
+            onBlur={email.validate}
+            error={email.error}
+          />
+
+          <Input
+            id="Subject"
+            label="Betreff"
+            type="text"
+            value={subject.value}
+            onChange={value => subject.setValue(value)}
+            onBlur={subject.validate}
+            error={subject.error}
+          />
+
+          <Input
+            id="Message"
+            label="Nachricht"
+            type="textbox"
+            value={message.value}
+            onChange={value => message.setValue(value)}
+            onBlur={message.validate}
+            error={message.error}
+            className={styles.message}
+          />
+        </ModalBody>
+        <ModalFooter>
+          <SecondaryButton ref={ref} onClick={closeModal}>
+            Abbrechen
+          </SecondaryButton>
+          <PrimaryButton onClick={() => {}}>Senden</PrimaryButton>
+        </ModalFooter>
+      </form>
+    </Modal>
+  );
+};
+
+export default TicketModal;
