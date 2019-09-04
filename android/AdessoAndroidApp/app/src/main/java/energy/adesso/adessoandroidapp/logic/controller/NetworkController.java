b1d2479f2bd60b1cec863a925e8076711d3462b5
Richard Hanß <33845166+finia2NA@users.noreply.github.com>
2019-09-04 11:28:47 +0200

    begin NetworkController

--- android/AdessoAndroidApp/app/src/main/java/energy/adesso/adessoandroidapp/logic/controller/NetworkController.java
+++ android/AdessoAndroidApp/app/src/main/java/energy/adesso/adessoandroidapp/logic/controller/NetworkController.java
@@ -1,6 +1,27 @@
 package energy.adesso.adessoandroidapp.logic.controller;
-//import javax.
+
+import android.content.SharedPreferences;
+
+import com.squareup.okhttp.OkHttpClient;
 
 public class NetworkController {
+  private String baseURL = "124.245.1.240:3001";
 
+  OkHttpClient ok = new OkHttpClient();
+
+
+  private static NetworkController instance;
+
+  private NetworkController() {
+
+  }
+
+  public synchronized void init(String baseURL) {
+    this.baseURL = baseURL;
+  }
+
+
+  public void setBaseURL(String baseURL) {
+    this.baseURL = baseURL;
+  }
 }
