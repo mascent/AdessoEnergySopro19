b626328d2b9ede801acb90d0c7da745b13ebe47f
Richard Hanß <33845166+finia2NA@users.noreply.github.com>
2019-09-04 11:52:28 +0200

    NetworkController

--- android/AdessoAndroidApp/app/src/main/java/energy/adesso/adessoandroidapp/logic/controller/NetworkController.java
+++ android/AdessoAndroidApp/app/src/main/java/energy/adesso/adessoandroidapp/logic/controller/NetworkController.java
@@ -2,7 +2,15 @@ package energy.adesso.adessoandroidapp.logic.controller;
 
 import android.content.SharedPreferences;
 
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+import energy.adesso.adessoandroidapp.logic.model.exception.NetworkException;
 
 public class NetworkController {
   private String baseURL = "124.245.1.240:3001";
@@ -12,12 +20,56 @@ public class NetworkController {
 
   private static NetworkController instance;
 
+  public static final MediaType JSON
+          = MediaType.parse("application/json; charset=utf-8");
+
+
   private NetworkController() {
 
   }
 
   public synchronized void init(String baseURL) {
     this.baseURL = baseURL;
+  }
+
+  public String get(String path) throws NetworkException {
+    Request request = new Request.Builder()
+            .url(baseURL + path)
+            .build();
+    try{
+      Response response = ok.newCall(request).execute();
+      return response.body().string();
+    }catch (IOException e){
+      throw new NetworkException();
+    }
+  }
+
+  public String post(String path, String json) throws NetworkException {
+    RequestBody body = RequestBody.create(JSON, json);
+    Request request = new Request.Builder()
+            .url(baseURL + path)
+            .post(body)
+            .build();
+    try{
+      Response response = ok.newCall(request).execute();
+      return response.body().string();
+    }catch (IOException e){
+      throw new NetworkException();
+    }
+  }
+
+  public String put(String path, String json) throws NetworkException {
+    RequestBody body = RequestBody.create(JSON, json);
+    Request request = new Request.Builder()
+            .url(baseURL + path)
+            .put(body)
+            .build();
+    try{
+      Response response = ok.newCall(request).execute();
+      return response.body().string();
+    }catch (IOException e){
+      throw new NetworkException();
+    }
   }
 
 
