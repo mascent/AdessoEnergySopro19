eb5e238cf47cf03188366ea3b15b3c7b70cbfd3a
Niklas Carstensen <stu211271@uni-kiel.de>
2019-09-05 10:16:00 +0200

    Add issue activity

---
+++ android/AdessoAndroidApp/app/src/main/java/energy/adesso/adessoandroidapp/ui/activities/IssueActivity.java
@@ -0,0 +1,16 @@
+package energy.adesso.adessoandroidapp.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import energy.adesso.adessoandroidapp.R;
+
+public class IssueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_issue);
+    }
+}
