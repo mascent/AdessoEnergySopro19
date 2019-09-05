9c770db7db9cd62f5270b63c373b1659b5502b23
Tammo Brüggemann <stu209220@mail.uni-kiel.de>
2019-09-05 11:23:02 +0200

    add readings and issues to index

--- web/src/services/ad-api/index.ts
+++ web/src/services/ad-api/index.ts
@@ -1,9 +1,13 @@
 import ApiConfig from './config';
 import MetersComponent from './meters-component';
 import UsersComponent from './users-component';
+import IssueComponent from './issues-component';
+import ReadingComponent from './readings-component';
 
 const config = new ApiConfig();
 const users = new UsersComponent(config);
 const meters = new MetersComponent(config);
+const issues = new IssueComponent(config);
+const readings = new ReadingComponent(config);
 
-export { config, users, meters };
+export { issues, readings, config, users, meters };
