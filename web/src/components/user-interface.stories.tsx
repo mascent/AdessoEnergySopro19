c2bc8546faea1fc8ff5e8800b9ad48da62c4c65e
Tammo Brüggemann <stu209220@mail.uni-kiel.de>
2019-09-06 10:12:12 +0200

    add skeleton user-interface without logic and test

---
+++ web/src/components/user-interface.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import UserInterface from './user-interface';
+
+storiesOf('Forms | UserInterface', module).add('Default', () => (
+  <UserInterface />
+));
