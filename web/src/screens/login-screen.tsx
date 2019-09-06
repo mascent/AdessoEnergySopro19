5b0d7a5d182dabf0b5f38c90c4bd77762003651e
Christoph Fricke <christoph@frickeonline.de>
2019-09-06 13:24:54 +0200

    Login now changes the Doc title

--- web/src/screens/login-screen.tsx
+++ web/src/screens/login-screen.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import LoginForm from '../components/login-form';
 import styles from './login-screen.module.scss';
 import { useAuth } from '../providers/authentication-provider';
+import useDocumentTitle from '../hooks/use-document-title';
 
 const LoginScreen: React.FC = () => {
+  useDocumentTitle('Login');
   const { login } = useAuth();
 
   return (
