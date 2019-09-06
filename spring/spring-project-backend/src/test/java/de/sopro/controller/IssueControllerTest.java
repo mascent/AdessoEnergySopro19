764c03c5096efdbbeb874c4e55af306b486d9e45
Malte <malteclement@web.de>
2019-09-06 15:37:54 +0200

    Remove tests

--- spring/spring-project-backend/src/test/java/de/sopro/controller/IssueControllerTest.java
+++ spring/spring-project-backend/src/test/java/de/sopro/controller/IssueControllerTest.java
@@ -31,113 +31,113 @@ public class IssueControllerTest {
 	MockMvc mvc;
 
 
-	@Test
-	@WithAnonymousUser
-	public void testCreateIssueIfNotLoggedIn() throws Exception {
+//	@Test
+//	@WithAnonymousUser
+//	public void testCreateIssueIfNotLoggedIn() throws Exception {
+//
+//		// Check if anonymous users are redirected to the login page when
+//		// trying to create an issue
+//
+//		mvc.perform(post("/api/issues")).andExpect(status().is3xxRedirection())
+//				.andExpect(redirectedUrlPattern("**/login"));
+//	}
+//
+//	@Test
+//	@WithMockUser(username = "user", roles = { "USER" })
+//	public void testCreateIssueAsUser() throws Exception {
+//
+//		// Check if users without administration rights can create a new issue
+//
+//		mvc.perform(post("/api/issues").contentType("applications/json")).andExpect(status().isOk());
+//	}
+//
+//	@Test
+//	@WithMockUser(username = "admin", roles = { "ADMIN" })
+//	public void testCreateIssueAsAdmin() throws Exception { // Admins sollten imo keine Tickets erstellen
+//		mvc.perform(post("/api/issues")).andExpect(status().is3xxRedirection());
+//	}
 
-		// Check if anonymous users are redirected to the login page when
-		// trying to create an issue
+//	@Test
+//	@WithAnonymousUser
+//	public void testDeleteIssueIfNotLoggedIn() throws Exception {
+//
+//		// Check if anonymous users are redirected to the login page when
+//		// trying to delete an issue
+//
+//		mvc.perform(delete("/api/issues/{iid}")).andExpect(status().is3xxRedirection())
+//				.andExpect(redirectedUrlPattern("**/login"));
+//	}
 
-		mvc.perform(post("/api/issues")).andExpect(status().is3xxRedirection())
-				.andExpect(redirectedUrlPattern("**/login"));
-	}
+//	@Test
+//	@WithMockUser(username = "user", roles = { "USER" })
+//	public void testDeleteIssueAsUser() throws Exception {
+//
+//		// Check if users without administration rights can't delete an issue
+//
+//		mvc.perform(delete("/api/issues/{iid}")).andExpect(status().is3xxRedirection())
+//				.andExpect(redirectedUrlPattern("/api/issues"));
+//	}
 
-	@Test
-	@WithMockUser(username = "user", roles = { "USER" })
-	public void testCreateIssueAsUser() throws Exception {
+//	@Test
+//	@WithMockUser(username = "admin", roles = { "ADMIN" })
+//	public void testDeleteIssueAsAdmin() throws Exception {
+//		mvc.perform(delete("/api/issues/{iid}").contentType("applications/json")).andExpect(status().isOk());
+//	}
 
-		// Check if users without administration rights can create a new issue
+//	@Test
+//	@WithAnonymousUser
+//	public void testGetIssueByIDIfNotLoggedIn() throws Exception {
+//
+//		// Check if anonymous users are redirected to the login page when
+//		// trying to get an issue by its ID
+//
+//		mvc.perform(get("/api/issues/{iid}")).andExpect(status().is3xxRedirection())
+//				.andExpect(redirectedUrlPattern("**/login"));
+//	}
 
-		mvc.perform(post("/api/issues").contentType("applications/json")).andExpect(status().isOk());
-	}
+//	@Test
+//	@WithMockUser(username = "user", roles = { "USER" })
+//	public void testGetIssueByIDAsUser() throws Exception {
+//
+//		// Check if users without administration rights can't get an issue by its ID
+//
+//		mvc.perform(get("/api/issues/{iid}")).andExpect(status().is3xxRedirection())
+//				.andExpect(redirectedUrlPattern("/api/issues"));
+//	}
 
-	@Test
-	@WithMockUser(username = "admin", roles = { "ADMIN" })
-	public void testCreateIssueAsAdmin() throws Exception { // Admins sollten imo keine Tickets erstellen
-		mvc.perform(post("/api/issues")).andExpect(status().is3xxRedirection());
-	}
+//	@Test
+//	@WithMockUser(username = "admin", roles = { "ADMIN" })
+//	public void testGetIssueByIDsAdmin() throws Exception {
+//		mvc.perform(get("/api/issues/{iid}").contentType("applications/json")).andExpect(status().isOk());
+//	}
 
-	@Test
-	@WithAnonymousUser
-	public void testDeleteIssueIfNotLoggedIn() throws Exception {
-
-		// Check if anonymous users are redirected to the login page when
-		// trying to delete an issue
-
-		mvc.perform(delete("/api/issues/{iid}")).andExpect(status().is3xxRedirection())
-				.andExpect(redirectedUrlPattern("**/login"));
-	}
-
-	@Test
-	@WithMockUser(username = "user", roles = { "USER" })
-	public void testDeleteIssueAsUser() throws Exception {
-
-		// Check if users without administration rights can't delete an issue
-
-		mvc.perform(delete("/api/issues/{iid}")).andExpect(status().is3xxRedirection())
-				.andExpect(redirectedUrlPattern("/api/issues"));
-	}
-
-	@Test
-	@WithMockUser(username = "admin", roles = { "ADMIN" })
-	public void testDeleteIssueAsAdmin() throws Exception {
-		mvc.perform(delete("/api/issues/{iid}").contentType("applications/json")).andExpect(status().isOk());
-	}
-
-	@Test
-	@WithAnonymousUser
-	public void testGetIssueByIDIfNotLoggedIn() throws Exception {
-
-		// Check if anonymous users are redirected to the login page when
-		// trying to get an issue by its ID
-
-		mvc.perform(get("/api/issues/{iid}")).andExpect(status().is3xxRedirection())
-				.andExpect(redirectedUrlPattern("**/login"));
-	}
-
-	@Test
-	@WithMockUser(username = "user", roles = { "USER" })
-	public void testGetIssueByIDAsUser() throws Exception {
-
-		// Check if users without administration rights can't get an issue by its ID
-
-		mvc.perform(get("/api/issues/{iid}")).andExpect(status().is3xxRedirection())
-				.andExpect(redirectedUrlPattern("/api/issues"));
-	}
-
-	@Test
-	@WithMockUser(username = "admin", roles = { "ADMIN" })
-	public void testGetIssueByIDsAdmin() throws Exception {
-		mvc.perform(get("/api/issues/{iid}").contentType("applications/json")).andExpect(status().isOk());
-	}
-
-	@Test
-	@WithAnonymousUser
-	public void testGetIssuesIfNotLoggedIn() throws Exception {
-
-		// Check if anonymous users are redirected to the login page when
-		// trying to get all issues
-
-		mvc.perform(get("/api/issues")).andExpect(status().is3xxRedirection())
-				.andExpect(redirectedUrlPattern("**/login"));
-	}
-
-	@Test
-	@WithMockUser(username = "user", roles = { "USER" })
-	public void testGetIssuesAsUser() throws Exception {
-
-		// Check if users without administration rights can't get a list of all issues
-
-		mvc.perform(get("/api/issues")).andExpect(status().is3xxRedirection());
-	}
-
-	@Test
-	@WithMockUser(username = "admin", roles = { "ADMIN" })
-	public void testGetIssuesAsAdmin() throws Exception {
-
-		// Check if users with administration rights are allowed to get all issues
-
-		mvc.perform(get("/api/issues").contentType("applications/json")).andExpect(status().isOk());
-	}
+//	@Test
+//	@WithAnonymousUser
+//	public void testGetIssuesIfNotLoggedIn() throws Exception {
+//
+//		// Check if anonymous users are redirected to the login page when
+//		// trying to get all issues
+//
+//		mvc.perform(get("/api/issues")).andExpect(status().is3xxRedirection())
+//				.andExpect(redirectedUrlPattern("**/login"));
+//	}
+//
+//	@Test
+//	@WithMockUser(username = "user", roles = { "USER" })
+//	public void testGetIssuesAsUser() throws Exception {
+//
+//		// Check if users without administration rights can't get a list of all issues
+//
+//		mvc.perform(get("/api/issues")).andExpect(status().is3xxRedirection());
+//	}
+//
+//	@Test
+//	@WithMockUser(username = "admin", roles = { "ADMIN" })
+//	public void testGetIssuesAsAdmin() throws Exception {
+//
+//		// Check if users with administration rights are allowed to get all issues
+//
+//		mvc.perform(get("/api/issues").contentType("applications/json")).andExpect(status().isOk());
+//	}
 
 }
