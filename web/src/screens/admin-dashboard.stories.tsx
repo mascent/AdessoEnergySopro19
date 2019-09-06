8b76815b702f3d63000286f92ab5118c30e7771e
Tammo Brüggemann <stu209220@mail.uni-kiel.de>
2019-09-06 14:35:40 +0200

    add Skeleton AdminDashBoard

---
+++ web/src/screens/admin-dashboard.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import AdminDashBoard from './admin-dashboard';
+
+storiesOf('Screens | Admin Dashboard', module).add('Default', () => (
+  <AdminDashBoard />
+));
