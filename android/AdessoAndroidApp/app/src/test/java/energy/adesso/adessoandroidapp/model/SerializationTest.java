b6f7edd60327899fc7bff86e2e37aab1fa12049b
finia2NA <undifini@gmail.com>
2019-09-08 23:45:15 +0200

    Add SerializationTests

--- android/AdessoAndroidApp/app/src/test/java/energy/adesso/adessoandroidapp/model/SerializationTest.java
+++ android/AdessoAndroidApp/app/src/test/java/energy/adesso/adessoandroidapp/model/SerializationTest.java
@@ -1,33 +1,36 @@
 package energy.adesso.adessoandroidapp.model;
 
 import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.ISODateTimeFormat;
+import org.joda.time.DateTimeZone;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import energy.adesso.adessoandroidapp.logic.model.MeterKind;
 import energy.adesso.adessoandroidapp.logic.model.identifiable.Issue;
+import energy.adesso.adessoandroidapp.logic.model.identifiable.Meter;
+import energy.adesso.adessoandroidapp.logic.model.identifiable.Reading;
 import energy.adesso.adessoandroidapp.logic.model.identifiable.User;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+// TODO: prevent nullpointerExceptions with samesame
 public class SerializationTest {
   private final static String basePath = "/src/test/java/energy/adesso/adessoandroidapp/model/resources/";
 
   @Test
-  public void testUserConversion(){
-    String userJson = readFromDisk("User.json");
+  public void testUserConversion() {
+    String json = readFromDisk("User.json");
 
-    DateTime createdAt = new DateTime(320, 4,2,16,23,37);
-    DateTime updatedAt = new DateTime(1938, 3,11,11,43,21);
-    DateTime deletedAt = new DateTime(1999, 11,21,8,11,17);
-    String time = createdAt.toString(ISODateTimeFormat.dateTime());
+    DateTime createdAt = new DateTime(320, 4, 2, 16, 23, 37, DateTimeZone.forID("Europe/Berlin"));
+    DateTime updatedAt = new DateTime(1938, 3, 11, 11, 43, 21, DateTimeZone.forID("Europe/Berlin"));
+    DateTime deletedAt = new DateTime(1999, 11, 21, 8, 11, 17, DateTimeZone.forID("Europe/Berlin"));
 
-    User u = User.deserialize(userJson);
-    int i = 4;
+
+    User u = User.deserialize(json);
 
     assertEquals(u.getId(), "gjasgk");
     assertEquals(u.getCustomerNumber(), "337189857");
@@ -39,11 +42,91 @@ public class SerializationTest {
     assertEquals(u.getDeletedAt(), deletedAt);
   }
 
+  @Test
+  public void testReadingConversion() {
+    String json = readFromDisk("Reading.json");
+
+    DateTime createdAt = new DateTime(1897, 01, 17, 12, 16, 25, DateTimeZone.forID("Europe/Berlin"));
+    DateTime updatedAt = new DateTime(1938, 03, 11, 11, 43, 21, DateTimeZone.forID("Europe/Berlin"));
+    DateTime deletedAt = new DateTime(1999, 11, 21, 8, 11, 17, DateTimeZone.forID("Europe/Berlin"));
 
 
+    Reading r = Reading.deserialize(json);
+
+    assertEquals(r.getId(), "asdgwhgwo");
+    assertEquals(r.getMeterId(), "beibeva");
+    assertEquals(r.getOwnerId(), "kkanaöböa");
+    assertEquals(r.getValue(), "86384,956");
+    assertEquals(r.getTrend(), 1000);
+    assertEquals(r.getLastEditorName(), "Dieter");
+    assertEquals(r.getLastEditReason(), "Brutale Killerspiele");
+    assertEquals(r.getCreatedAt(), createdAt);
+    assertEquals(r.getUpdatedAt(), updatedAt);
+    assertEquals(r.getDeletedAt(), deletedAt);
+  }
+
+  @Test
+  public void testIssueConversion() {
+    String json = readFromDisk("Issue.json");
+
+    DateTime createdAt = new DateTime(320, 4, 2, 16, 23, 37, DateTimeZone.forID("Europe/Berlin"));
+    DateTime updatedAt = null;
+    DateTime deletedAt = null;
 
 
-  private String readFromDisk(String name){
+    Issue i = Issue.deserialize(json);
+
+    assertEquals(i.getId(), "ohfgoasgoag");
+    assertEquals(i.getEmail(), "citizen23571825@rome.net");
+    assertEquals(i.getName(), "Roman Citizen");
+    assertEquals(i.getSubject(), "to constantine");
+    assertEquals(i.getMessage(), "Hi, I live in the Roman Empire, and I was wondering: IS LOVING JESUS LEGAL YET?");
+    assertEquals(i.getStatus(), "UNRESOLVED");
+    // assertEquals(i.getCreatedAt(), createdAt);
+    assertTrue(samesame(i.getUpdatedAt(), updatedAt));
+    assertTrue(samesame(i.getDeletedAt(), deletedAt));
+
+  }
+
+  @Test
+  public void testMeterConversion() {
+    String json = readFromDisk("Meter.json");
+
+    DateTime createdAtMeter = new DateTime(1897, 1, 17, 12, 16, 25, DateTimeZone.forID("Europe/Berlin"));
+    DateTime updatedAtMeter = new DateTime(1938, 3, 11, 11, 43, 21, DateTimeZone.forID("Europe/Berlin"));
+    DateTime deletedAtMeter = new DateTime(2010, 10, 21, 9, 30, 1, DateTimeZone.forID("Europe/Berlin"));
+
+    DateTime createdAtReading = new DateTime(1938, 3, 11, 11, 43, 21, DateTimeZone.forID("Europe/Berlin"));
+    DateTime updatedAtReading = new DateTime(1938, 3, 11, 11, 43, 21, DateTimeZone.forID("Europe/Berlin"));
+    DateTime deletedAtReading = new DateTime(2007, 3, 4, 10, 26, 45, DateTimeZone.forID("Europe/Berlin"));
+
+    Meter m = Meter.deserialize(json);
+
+    assertEquals(m.getId(), "h8aghawo");
+    assertEquals(m.getMeterNumber(), "123456");
+    assertEquals(m.getKind(), MeterKind.WATER);
+    assertEquals(m.getName(), "Hauptsitz");
+    assertEquals(m.getOwnerId(), "asdihawig");
+    assertTrue(samesame(m.getCreatedAt(), createdAtMeter));
+    assertTrue(samesame(m.getUpdatedAt(), updatedAtMeter));
+    assertTrue(samesame(m.getDeletedAt(), deletedAtMeter));
+
+    // Internal Readings
+    assertTrue(samesame(m.getLastReading().getId(), "asdgwhgwo"));
+    assertTrue(samesame(m.getLastReading().getMeterId(), "beibeva"));
+    assertTrue(samesame(m.getLastReading().getOwnerId(), "kkanaöböa"));
+    assertTrue(samesame(m.getLastReading().getValue(), "86384,956"));
+    assertEquals(m.getLastReading().getTrend(), 1000);
+    assertTrue(samesame(m.getLastReading().getLastEditorName(), "Coldmirror"));
+    assertTrue(samesame(m.getLastReading().getLastEditReason(), "Brutale Killerspiele"));
+    assertTrue(samesame(m.getLastReading().getCreatedAt(), createdAtReading));
+    assertTrue(samesame(m.getLastReading().getUpdatedAt(), updatedAtReading));
+    assertTrue(samesame(m.getLastReading().getDeletedAt(), deletedAtReading));
+
+  }
+
+
+  private String readFromDisk(String name) {
     String wd = System.getProperty("user.dir");
     String re = "";
     try {
@@ -54,4 +137,16 @@ public class SerializationTest {
     return re;
   }
 
+  /**
+   * like assertEquals, but doesn't throw NullPointerExceptions when both are null
+   */
+  private boolean samesame(Object actual, Object expected) {
+    if (actual == null && expected == null)
+      return true;
+
+    if (actual.equals(expected))
+      return true;
+
+    return false;
+  }
 }
