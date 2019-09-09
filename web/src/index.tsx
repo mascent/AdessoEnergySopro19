7d78d5619b164066203d43f8714fa27c269c63a8
Christoph Fricke <christoph@frickeonline.de>
2019-09-09 09:48:26 +0200

    Remove stories from coverage

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,6 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import * as serviceWorker from './service-worker';
 import './index.scss';
 import { AuthenticationProvider } from './providers/authentication-provider';
 import { config } from './services/ad-api';
@@ -19,8 +18,3 @@ ReactDOM.render(
   </StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
