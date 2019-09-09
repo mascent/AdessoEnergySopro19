044a46f6a5c386e30754bef1bd07121e2d2be5d9
Vincent Shahriyari <vincent@innovandalism.eu>
2019-09-09 11:29:11 +0200

    Fix Issue Controller

--- spring/spring-project-backend/src/main/java/de/sopro/repository/PersonRepository.java
+++ spring/spring-project-backend/src/main/java/de/sopro/repository/PersonRepository.java
@@ -10,6 +10,4 @@ public interface PersonRepository extends CrudRepository<Person, Long> {
 
 	public Optional<Person> findByUsername(String username);
 
-	public void deleteByUsername(String username);
-
 }
