3d4b896b542031719cd8b20683d8c0e420798ba3
Christoph Fricke <christoph@frickeonline.de>
2019-09-09 11:33:46 +0200

    Add test for isEmail validator

--- web/src/lib/validator.test.tsx
+++ web/src/lib/validator.test.tsx
@@ -1,16 +1,39 @@
 import { isStringEmpty } from './validators';
+import isEmail from 'validator/lib/isEmail';
 
-test('is true for an empty string or only spaces', () => {
-  expect(isStringEmpty('')).toBeTruthy();
-  expect(isStringEmpty('        ')).toBeTruthy();
+describe('isStringEmpty function', () => {
+  test('is true for an empty string or only spaces', () => {
+    expect(isStringEmpty('')).toBeTruthy();
+    expect(isStringEmpty('        ')).toBeTruthy();
+  });
+
+  test('is false for a string with characters', () => {
+    expect(isStringEmpty('djskdsjd')).toBeFalsy();
+    expect(isStringEmpty('    djsjd  d   ')).toBeFalsy();
+  });
+
+  test('is false for a string of numbers', () => {
+    expect(isStringEmpty('162173672')).toBeFalsy();
+    expect(isStringEmpty('  1343  242   ')).toBeFalsy();
+  });
 });
 
-test('is false for a string with characters', () => {
-  expect(isStringEmpty('djskdsjd')).toBeFalsy();
-  expect(isStringEmpty('    djsjd  d   ')).toBeFalsy();
-});
+describe('isEmail function', () => {
+  test('is false for an empty email', () => {
+    expect(isEmail('')).toBeFalsy();
+    expect(isEmail('        ')).toBeFalsy();
+  });
 
-test('is false for a string of numbers', () => {
-  expect(isStringEmpty('162173672')).toBeFalsy();
-  expect(isStringEmpty('  1343  242   ')).toBeFalsy();
+  const cases = [
+    ['a@b.de', true],
+    ['d', false],
+    ['  a@b.de', false],
+    ['a@b.d', false],
+    ['hello@games.com', true]
+  ];
+
+  test.each(cases)('email %s is %s', (mail, result) => {
+    /// @ts-ignore TS things both are string | boolean. Don't know why...
+    expect(isEmail(mail)).toBe(result);
+  });
 });
