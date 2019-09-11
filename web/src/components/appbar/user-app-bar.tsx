cd47ad210ddcf94ca4cc8bbb81c07e96619db31f
Tammo Brüggemann <stu209220@mail.uni-kiel.de>
2019-09-11 17:06:04 +0200

    starting ticket modal

--- web/src/components/appbar/user-app-bar.tsx
+++ web/src/components/appbar/user-app-bar.tsx
@@ -4,25 +4,32 @@ import EmailOutline from 'mdi-react/EmailOutlineIcon';
 import Logout from 'mdi-react/LogoutIcon';
 import { Title } from '../generics/text';
 import { InvButton } from '../generics/button';
+import TicketModal from '../ticket-modal';
 
 const UserAppBar: React.FC = () => {
+  const [isOpen, setOpen] = React.useState(false);
+  const closeButton = React.useRef<HTMLButtonElement>(null);
+
   return (
-    <div className={styles.userAppBar}>
-      <nav className={styles.navigation}>
-        <Title className={styles.title}>adesso energy</Title>
-        <div>
-          <InvButton
-            className={styles.emailButton}
-            onClick={() => console.log('test')}
-          >
-            <EmailOutline />
-          </InvButton>
-          <InvButton onClick={() => console.log('test')}>
-            <Logout />
-          </InvButton>
-        </div>
-      </nav>
-    </div>
+    <>
+      <div className={styles.userAppBar}>
+        <nav className={styles.navigation}>
+          <Title className={styles.title}>adesso energy</Title>
+          <div>
+            <InvButton
+              className={styles.emailButton}
+              onClick={() => setOpen(true)}
+            >
+              <EmailOutline />
+            </InvButton>
+            <InvButton onClick={() => console.log('test')}>
+              <Logout />
+            </InvButton>
+          </div>
+        </nav>
+      </div>
+      <TicketModal isOpen={isOpen} closeModal={() => {}} onSend={() => {}} />
+    </>
   );
 };
 
