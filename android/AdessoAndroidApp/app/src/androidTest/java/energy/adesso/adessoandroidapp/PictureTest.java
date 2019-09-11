55d1b90b72ce40e9a0d4190dee3123dc1e88ac8d
Niklas Carstensen <stu211271@uni-kiel.de>
2019-09-11 14:16:39 +0200

    Beautify photoTest

--- android/AdessoAndroidApp/app/src/androidTest/java/energy/adesso/adessoandroidapp/PictureTest.java
+++ android/AdessoAndroidApp/app/src/androidTest/java/energy/adesso/adessoandroidapp/PictureTest.java
@@ -29,12 +29,14 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.AllOf.allOf;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PictureTest {
   @Rule
   public IntentsTestRule<MainActivity> mActivity = new IntentsTestRule<MainActivity>(MainActivity.class);
 
+  // We only test photos here because the code is nearly identical for gallery calls besides the intent action
   @Test
   public void takePhoto() {
     // Create a bitmap we can use for our simulated camera image
@@ -47,14 +49,17 @@ public class PictureTest {
     resultData.putExtra("data", icon);
     Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
 
-    // Stub out the Camera. When an intent is sent to the Camera, this tells Espresso to respond
-    // with the ActivityResult we just created
+    // Stub the Camera Intent call
     intending(hasAction(equalTo(MediaStore.ACTION_IMAGE_CAPTURE))).respondWith(result);
 
+    // Trigger the onPhotoButton event
     onView(allOf(withId(R.id.fab), isDisplayed())).perform(click());
     onView(allOf(withText(R.string.take_photo), isDisplayed())).perform(click());
 
-    // We can also validate that an intent resolving to the "camera" activity has been sent out by our app
+    // Validate that an camera intent has been sent
     intended(hasAction(equalTo(MediaStore.ACTION_IMAGE_CAPTURE)));
+
+    // Check for crashes
+    assertFalse(mActivity.getActivity().isFinishing());
   }
 }
