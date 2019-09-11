2c93cfdb468ce0989454bb0228fea96d4ddd3ebb
Christoph Fricke <christoph@frickeonline.de>
2019-09-11 14:34:26 +0200

    Style graph

--- web/src/components/graph/graph.tsx
+++ web/src/components/graph/graph.tsx
@@ -14,20 +14,20 @@ function createData(title: string, data: number[], dates: string[]) {
         label: title,
         fill: false,
         lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
+        backgroundColor: '#f0b429',
+        borderColor: '#f0b429',
         borderCapStyle: 'butt',
         borderDash: [],
         borderDashOffset: 0.0,
         borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
+        pointBorderColor: '#cb6e17',
+        pointBackgroundColor: '#fce588',
         pointBorderWidth: 1,
         pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBackgroundColor: '#f0b429',
+        pointHoverBorderColor: '#f0b429',
         pointHoverBorderWidth: 2,
-        pointRadius: 1,
+        pointRadius: 3,
         pointHitRadius: 10,
         data
       }
@@ -36,12 +36,7 @@ function createData(title: string, data: number[], dates: string[]) {
 }
 
 const Graph: React.FC<graphProps> = ({ data, title, dates }) => {
-  return (
-    <div>
-      <h2>Line Example</h2>
-      <Line data={createData(title, data, dates)} />
-    </div>
-  );
+  return <Line data={createData(title, data, dates)} />;
 };
 
 export default Graph;
