db1f7ff7c8f54ede89c4d0c3b3b89348baaa3195
Christoph Fricke <christoph@frickeonline.de>
2019-09-12 13:18:45 +0200

    Remove fakes from api abstraction

--- web/src/services/ad-api/meters-component.ts
+++ web/src/services/ad-api/meters-component.ts
@@ -1,42 +1,38 @@
 import AdessoEnergyApiComponent from './abstract-api-component';
 import { MeterDTO } from '../../typings/dtos';
-import { buildList, buildMeterDTO } from '../../utils/fake-builder';
 
 class MetersComponent extends AdessoEnergyApiComponent {
   public async getMetersForUser(id: string): Promise<MeterDTO[]> {
-    return buildList(buildMeterDTO, 5, 100);
-    // const result = await this.get(`/api/users/${id}/meters`);
+    const result = await this.get(`/api/users/${id}/meters`);
 
-    // if (!result.ok) {
-    //   throw new Error('A problem occurred with the request.');
-    // }
+    if (!result.ok) {
+      throw new Error('A problem occurred with the request.');
+    }
 
-    // return await result.json();
+    return await result.json();
   }
 
   public async createNewMeter(meter: Partial<MeterDTO>): Promise<MeterDTO> {
-    return buildMeterDTO(meter);
-    // const result = await this.post('/api/meters', meter);
+    const result = await this.post('/api/meters', meter);
 
-    // if (!result.ok) {
-    //   throw new Error('A problem occurred with the request.');
-    // }
+    if (!result.ok) {
+      throw new Error('A problem occurred with the request.');
+    }
 
-    // return await result.json();
+    return await result.json();
   }
 
   public async updateMeter(
     id: string,
     meter: Partial<MeterDTO>
   ): Promise<MeterDTO> {
-    return buildMeterDTO(meter);
-    // const result = await this.put(`/api/meters/${id}`, meter);
+    const result = await this.put(`/api/meters/${id}`, meter);
 
-    // if (!result.ok) {
-    //   throw new Error('A problem occurred with the request.');
-    // }
+    if (!result.ok) {
+      throw new Error('A problem occurred with the request.');
+    }
 
-    // return await result.json();
+    return await result.json();
   }
 }
 
--- web/src/services/ad-api/readings-component.ts
+++ web/src/services/ad-api/readings-component.ts
@@ -1,31 +1,28 @@
 import AdessoEnergyApiComponent from './abstract-api-component';
 import { ReadingDTO } from '../../typings/dtos';
-import { buildList, buildReadingDTO } from '../../utils/fake-builder';
 
 class ReadingComponent extends AdessoEnergyApiComponent {
   public async getAllReadingsByAMeter(id: string): Promise<ReadingDTO[]> {
-    return buildList(buildReadingDTO, 5, 100);
-    // const result = await this.get(`/api/meters/${id}/readings`);
+    const result = await this.get(`/api/meters/${id}/readings`);
 
-    // if (!result.ok) {
-    //   throw new Error('A problem occurred with the request.');
-    // }
+    if (!result.ok) {
+      throw new Error('A problem occurred with the request.');
+    }
 
-    // return await result.json();
+    return await result.json();
   }
 
   public async addReadingToAMeter(
     id: string,
     reading: Partial<ReadingDTO>
   ): Promise<ReadingDTO> {
-    return buildReadingDTO(reading);
-    // const result = await this.post(`/api/meters/${id}/readings`, reading);
+    const result = await this.post(`/api/meters/${id}/readings`, reading);
 
-    // if (!result.ok) {
-    //   throw new Error('A problem occurred with the request.');
-    // }
+    if (!result.ok) {
+      throw new Error('A problem occurred with the request.');
+    }
 
-    // return await result.json();
+    return await result.json();
   }
 
   public async updateReadingForAMeter(
@@ -33,17 +30,16 @@ class ReadingComponent extends AdessoEnergyApiComponent {
     readingID: string,
     update: Partial<ReadingDTO>
   ): Promise<ReadingDTO> {
-    return buildReadingDTO(update);
-    // const result = await this.put(
-    //   `/api/meters/${meterID}/readings/${readingID}`,
-    //   update
-    // );
+    const result = await this.put(
+      `/api/meters/${meterID}/readings/${readingID}`,
+      update
+    );
 
-    // if (!result.ok) {
-    //   throw new Error('A problem occurred with the request.');
-    // }
+    if (!result.ok) {
+      throw new Error('A problem occurred with the request.');
+    }
 
-    // return await result.json();
+    return await result.json();
   }
 }
 
