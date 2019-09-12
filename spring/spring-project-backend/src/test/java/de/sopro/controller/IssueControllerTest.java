f2cd51867522a792cae350f1b48625b40f2a0ed5
Mattis thor Straten <stu210094@mail.uni-kiel.de>
2019-09-12 11:19:11 +0200

    Add correct tests.

--- spring/spring-project-backend/src/test/java/de/sopro/controller/IssueControllerTest.java
+++ spring/spring-project-backend/src/test/java/de/sopro/controller/IssueControllerTest.java
@@ -8,6 +8,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
@@ -18,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -29,10 +31,14 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import de.sopro.data.Issue;
+import de.sopro.data.Person;
+import de.sopro.data.Role;
+import de.sopro.data.User;
 import de.sopro.dto.IssueDTO;
 import de.sopro.repository.IssueRepository;
 import de.sopro.repository.PersonRepository;
 import de.sopro.repository.UserRepository;
+import okhttp3.Credentials;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -42,10 +48,10 @@ public class IssueControllerTest {
 	@Autowired
 	MockMvc mvc;
 
-	@MockBean
+	@Autowired
 	UserRepository userRepository;
 
-	@MockBean
+	@Autowired
 	PersonRepository personRepository;
 
 	@Autowired
@@ -56,18 +62,46 @@ public class IssueControllerTest {
 
 	Issue i;
 	Long testIssueId;
+	User user;
+	Person admin;
+	String userCredentials;
+	String adminCredentials;
 
 	@BeforeEach
 	public void createEntrys() {
-		i = new Issue("mts", "mattists97@web.de", "Test", "Hallo. Hallo. Test. Test.", (long) 1);
+		String userPassword = "password";
+		String adminPassword = "password";
+		if (userRepository.findByUsername("fwm").orElse(null) != null) {
+			userRepository.deleteById(userRepository.findByUsername("fwm").orElse(null).getPersonId());
+		}
+
+		user = new User("frank", "white", "frank@white.mercedes", "fwm", passwordEncoder.encode(userPassword),
+				Role.User);
+		userRepository.save(user);
+
+		if (personRepository.findByUsername("fler").orElse(null) != null) {
+			personRepository.deleteById(personRepository.findByUsername("fler").orElse(null).getPersonId());
+		}
+
+		admin = new Person("fler", passwordEncoder.encode(adminPassword), Role.Admin);
+		personRepository.save(admin);
+
+		i = new Issue("fwm", "frank@white.mercedes", "Flizzy", "Warum denn Mercedes?", user.getPersonId());
 		issueRepository.save(i);
 		testIssueId = i.getIssueId();
+
+		userCredentials = Credentials.basic("fwm", userPassword);
+		adminCredentials = Credentials.basic("fler", adminPassword);
 	}
 
 	@AfterEach
 	public void removeEntrys() {
-		if (i != null) {
-			issueRepository.delete(i);
+		issueRepository.deleteAll();
+		if (user != null) {
+			userRepository.delete(user);
+		}
+		if (admin != null) {
+			personRepository.delete(admin);
 		}
 	}
 
@@ -81,35 +115,37 @@ public class IssueControllerTest {
 	}
 
 	@Test
-	@WithMockUser(username = "user", roles = { "User", "Shared" })
+//	@WithMockUser(username = "user", roles = { "User", "Shared" })
 	public void testCreateIssueAsUser() throws Exception {
 
 		// Check if users without administration rights can create a new issue
 
-		MvcResult result = mvc.perform(post("/api/issues").param("name", "fw37").param("email", "frank-white@bmw.com")
-				.param("subject", "Frank White").param("description", "jagt euch mit dem BMW BMW.")
-				.contentType("applications/json")).andExpect(status().isOk()).andReturn();
+		MvcResult result = mvc
+				.perform(post("/api/issues").header(HttpHeaders.AUTHORIZATION, userCredentials).param("name", "fw37")
+						.param("email", "frank-white@bmw.com").param("subject", "Frank White")
+						.param("description", "jagt euch mit dem BMW BMW.").contentType("applications/json"))
+				.andExpect(status().isOk()).andReturn();
 
 		// cleaning up by deleting the new created issue
 
 		String s = result.getResponse().getContentAsString();
 		IssueDTO issue = new Gson().fromJson(s, IssueDTO.class);
-		assertEquals(issue.getSubject(), "Frank White"); // TODO fix nullpointer, ist leer weil s leer ist
+		assertEquals(issue.getSubject(), "Frank White");
 		if (issue != null) {
 			issueRepository.deleteById(issue.getId());
 		}
 	}
 
 	@Test
-	@WithMockUser(username = "user", roles = { "User", "Shared" })
+//	@WithMockUser(username = "user", roles = { "User", "Shared" })
 	public void testCreateIssueAsUserWithMissingEmail() throws Exception {
 
 		// Check if users without administration rights can't create a new issue with
 		// missing arguments
 
-		MvcResult result = mvc
-				.perform(post("/api/issues").param("name", "fw37").param("subject", "Frank White")
-						.param("description", "jagt euch mit dem BMW BMW.").contentType("applications/json"))
+		MvcResult result = mvc.perform(post("/api/issues").header(HttpHeaders.AUTHORIZATION, userCredentials)
+				.param("name", "fw37").param("subject", "Frank White")
+				.param("description", "jagt euch mit dem BMW BMW.").contentType("applications/json"))
 				.andExpect(status().is4xxClientError()).andReturn();
 
 		// check if issue isn't existing
@@ -119,13 +155,14 @@ public class IssueControllerTest {
 	}
 
 	@Test
-	@WithMockUser(username = "admin", roles = { "Admin", "Shared" })
+//	@WithMockUser(username = "admin", roles = { "Admin", "Shared" })
 	public void testCreateIssueAsAdmin() throws Exception {
 
 		// Check if administrators can't create a new issue because there is no reason
 		// they would ever do this
 
-		mvc.perform(post("/api/issues")).andExpect(status().is4xxClientError());
+		mvc.perform(post("/api/issues").header(HttpHeaders.AUTHORIZATION, adminCredentials))
+				.andExpect(status().is4xxClientError());
 	}
 
 	@Test
@@ -138,34 +175,29 @@ public class IssueControllerTest {
 	}
 
 	@Test
-	@WithMockUser(username = "user", roles = { "User", "Shared" })
+//	@WithMockUser(username = "user", roles = { "User", "Shared" })
 	public void testGetIssuesAsUser() throws Exception {
 
 		// Check if users without administration rights can't get a list of all issues
 
-		mvc.perform(get("/api/issues")).andExpect(status().is4xxClientError());
+		mvc.perform(get("/api/issues").header(HttpHeaders.AUTHORIZATION, userCredentials))
+				.andExpect(status().is4xxClientError());
 	}
 
 	@Test
-	@WithMockUser(username = "admin", roles = { "Admin", "Shared" })
+//	@WithMockUser(username = "admin", roles = { "Admin", "Shared" })
 	public void testGetIssuesAsAdmin() throws Exception {
 
 		// Check if users with administration rights are allowed to get a list of all
 		// issues
 
-		MvcResult result = mvc.perform(get("/api/issues").contentType("applications/json")).andExpect(status().isOk())
-				.andReturn();
+		MvcResult result = mvc.perform(
+				get("/api/issues").header(HttpHeaders.AUTHORIZATION, adminCredentials).contentType("applications/json"))
+				.andExpect(status().isOk()).andReturn();
 		Type listType = new TypeToken<List<IssueDTO>>() {}.getType();
-		String s = result.getResponse().getContentAsString(); //wird hier auch empty sein
+		String s = result.getResponse().getContentAsString();
 		List<IssueDTO> issues = new Gson().fromJson(s, listType);
-		assertTrue(issues.contains(new IssueDTO(i))); // TODO fix assertion, issues wird leer sein, liegt an Auslesen
-														// mit Gson (wie oben)
-//		List<IssueDTO> expectedIssues = new ArrayList<>();
-//		if (i != null) {
-//			IssueDTO iDTO = new IssueDTO(i);
-//			expectedIssues.add(iDTO);
-//		}
-//		assertEquals(issues, expectedIssues);
+		assertTrue(issues.contains(new IssueDTO(i))); // TODO fix
 	}
 
 	@Test
@@ -178,25 +210,29 @@ public class IssueControllerTest {
 	}
 
 	@Test
-	@WithMockUser(username = "user", roles = { "User", "Shared" })
+//	@WithMockUser(username = "user", roles = { "User", "Shared" })
 	public void testCloseIssueAsUser() throws Exception {
 
 		// Check if users without administration rights can't close an issue
 
-		mvc.perform(put("/api/issues" + testIssueId)).andExpect(status().is4xxClientError());
+		mvc.perform(put("/api/issues" + testIssueId).header(HttpHeaders.AUTHORIZATION, userCredentials))
+				.andExpect(status().is4xxClientError());
 	}
 
 	@Test
-	@WithMockUser(username = "admin", roles = { "Admin", "Shared" })
+//	@WithMockUser(username = "admin", roles = { "Admin", "Shared" })
 	public void testCloseIssueAsAdmin() throws Exception {
 
 		// CHeck if users with administration rights can close an issue
 
-		mvc.perform(
-				put("/api/issues/" + testIssueId).param("iid", testIssueId.toString()).contentType("applications/json"))
-				.andExpect(status().isOk());
-		assertTrue(i.getCloserId() != null); // TODO gemockter User hat keine ID, die ausgelesen werden kann. Hier
-												// Rückgabe mocken?
+		MvcResult result = mvc
+				.perform(put("/api/issues/" + testIssueId).header(HttpHeaders.AUTHORIZATION, adminCredentials)
+						.param("iid", testIssueId.toString()).contentType("applications/json"))
+				.andExpect(status().isOk()).andReturn();
+		Boolean succes = new Gson().fromJson(result.getResponse().getContentAsString(), Boolean.class);
+		assertEquals(succes, true);
+		Issue i1 = issueRepository.findById(testIssueId).orElse(null);
+		assertEquals(i1.getCloserId(), admin.getPersonId());
 	}
 
 	@Test
@@ -209,27 +245,31 @@ public class IssueControllerTest {
 	}
 
 	@Test
-	@WithMockUser(username = "user", roles = { "User", "Shared" })
+//	@WithMockUser(username = "user", roles = { "User", "Shared" })
 	public void testGetIssueByIDAsUser() throws Exception {
 
 		// Check if users without administration rights can't get an issue by its ID
 
-		mvc.perform(get("/api/issues" + testIssueId.toString())).andExpect(status().is4xxClientError());
+		mvc.perform(get("/api/issues" + testIssueId.toString()).header(HttpHeaders.AUTHORIZATION, userCredentials))
+				.andExpect(status().is4xxClientError());
 	}
 
 	@Test
-	@WithMockUser(username = "admin", roles = { "Admin", "Shared" })
+//	@WithMockUser(username = "admin", roles = { "Admin", "Shared" })
 	public void testGetIssueByIDsAdmin() throws Exception {
 
 		// Check if users with administration rights can get an issue by its ID
 
-		MvcResult result = mvc.perform(get("/api/issues/" + testIssueId.toString()).param("iid", testIssueId.toString())
-				.contentType("applications/json")).andExpect(status().isOk()).andReturn();
-		IssueDTO issue = new Gson().fromJson(result.getResponse().getContentAsString(), IssueDTO.class); //TODO verstehen warum es hier geht
-		assertEquals(issue.getName(), "mts");
-		assertEquals(issue.getEmail(), "mattists97@web.de");
-		assertEquals(issue.getSubject(), "Test");
-		assertEquals(issue.getDescription(), "Hallo. Hallo. Test. Test.");
+		MvcResult result = mvc
+				.perform(
+						get("/api/issues/" + testIssueId.toString()).header(HttpHeaders.AUTHORIZATION, adminCredentials)
+								.param("iid", testIssueId.toString()).contentType("applications/json"))
+				.andExpect(status().isOk()).andReturn();
+		IssueDTO issue = new Gson().fromJson(result.getResponse().getContentAsString(), IssueDTO.class);
+		assertEquals(issue.getName(), "fwm");
+		assertEquals(issue.getEmail(), "frank@white.mercedes");
+		assertEquals(issue.getSubject(), "Flizzy");
+		assertEquals(issue.getDescription(), "Warum denn Mercedes?");
 		assertEquals(issue.getIsClosed(), false);
 	}
 
