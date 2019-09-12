8386d39aa626fb413d4f13353d2e9f574e6dec0c
Malte <malteclement@web.de>
2019-09-12 18:18:49 +0200

    commit before merge

--- spring/spring-project-backend/src/main/java/de/sopro/dto/PictureResponseDTO.java
+++ spring/spring-project-backend/src/main/java/de/sopro/dto/PictureResponseDTO.java
@@ -17,4 +17,28 @@ public class PictureResponseDTO {
 		this.value = value;
 	}
 
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Long getMid() {
+		return mid;
+	}
+
+	public void setMid(Long mid) {
+		this.mid = mid;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
 }
