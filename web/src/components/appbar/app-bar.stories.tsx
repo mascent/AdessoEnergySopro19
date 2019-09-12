6458136cb8263d0f7bf5d3183b3c19e7733f2ca3
Christoph Fricke <christoph@frickeonline.de>
2019-09-12 17:40:21 +0200

    Fix stories

--- web/src/components/appbar/app-bar.stories.tsx
+++ web/src/components/appbar/app-bar.stories.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import UserAppBar from './user-app-bar';
 import AdminAppBar from './admin-app-bar';
+import { AuthenticationProvider } from '../../providers/authentication-provider';
+import { IssuesProvider } from '../../providers/issues-provider';
 
 storiesOf('Form | Appbar', module)
-  .add('User', () => <UserAppBar />)
-  .add('Admin', () => <AdminAppBar selected="users" />);
+  .add('User', () => (
+    <IssuesProvider>
+      <AuthenticationProvider>
+        <UserAppBar />
+      </AuthenticationProvider>
+    </IssuesProvider>
+  ))
+  .add('Admin', () => (
+    <AuthenticationProvider>
+      <AdminAppBar selected="users" />
+    </AuthenticationProvider>
+  ));
--- web/src/components/user-dashboard.stories.tsx
+++ web/src/components/user-dashboard.stories.tsx
@@ -4,25 +4,40 @@ import UserDashboard from './user-dashboard';
 import { MetersProvider } from '../providers/meters-provider';
 import { withKnobs, boolean } from '@storybook/addon-knobs';
 import { Meter } from '../typings/provider-data-interfaces';
-import { buildMeter, buildList } from '../utils/fake-builder';
+import { buildMeter, buildList, buildReading } from '../utils/fake-builder';
 import { WithRouter } from '../utils/with-router';
+import { AuthenticationProvider } from '../providers/authentication-provider';
+import { ReadingsProvider } from '../providers/readings-provider';
 
 const meters: Meter[] = buildList(buildMeter, 5, 100);
+const readings: Reading[] = buildList(buildReading, 5, 100);
 
 storiesOf('Dashboards | UserDashboard', module)
   .addDecorator(withKnobs)
   .add('default', () => (
     <WithRouter>
-      <MetersProvider
-        override={{
-          isLoading: boolean('Loading', false),
-          meters: meters,
-          addMeter: async () => true,
-          fetchMeters: async () => true,
-          updateMeter: async () => true
-        }}
-      >
-        <UserDashboard />
-      </MetersProvider>
+      <AuthenticationProvider>
+        <MetersProvider
+          override={{
+            isLoading: boolean('Loading', false),
+            meters: meters,
+            addMeter: async () => true,
+            fetchMeters: async () => true,
+            updateMeter: async () => true
+          }}
+        >
+          <ReadingsProvider
+            override={{
+              isLoading: boolean('Loading', false),
+              readings: readings,
+              addReading: async () => true,
+              fetchReadings: async () => true,
+              updateReading: async () => true
+            }}
+          >
+            <UserDashboard />
+          </ReadingsProvider>
+        </MetersProvider>
+      </AuthenticationProvider>
     </WithRouter>
   ));
